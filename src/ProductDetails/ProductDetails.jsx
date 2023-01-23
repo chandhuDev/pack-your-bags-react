bb5af040b08b3485fc0babb414f1fab6bedb3005
chandhu <chandhudev0@gmail.com>
2023-01-23 23:31:55 +0530

    deleted input file as it is unwannted

--- src/ProductDetails/ProductDetails.jsx
+++ src/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useParams,useLocation } from 'react-router-dom';
 import { useQuery, gql,useMutation } from '@apollo/client';
-
+import Input from '../Input/Input';
 
 
 
@@ -33,12 +33,12 @@ const ProductDetails = () => {
   return (
     <>
       <div className='h-screen w-full px-16 py-10 border-r-4 border-red-400 rounded-md'>
-          <div className='flex flex-row px-2 py-2 justify-center items-center'>
-             <Input />
-             <Input />
+          <div className='flex flex-row px-2 py-2 justify-evenly mx-40 items-center bg-pink-300'>
+             <h4 className='text-lg font-serif text-center text-blue-900 py-6'>Enter your source</h4>
+             <h4 className='text-lg font-serif text-center text-blue-900 py-6'>Enter your destination</h4>
           </div>
-          <div className='w-full h-max my-3 p-4 border-r-2 rounded-lg border-blue-800'>
-           <h4 className='font-serif text-right font-medium'></h4>
+          <div className='w-full h-max my-3 p-4 border-r-2 rounded-lg border-blue-800 bg-slate-700'>
+           <h4 className='font-serif text-right font-medium'>hello</h4>
           </div>
           
       </div>
